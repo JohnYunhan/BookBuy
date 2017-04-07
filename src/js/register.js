375a27b324e7e891980b35339806811edf2250c3
John-PC\John <18702516203@126.com>
2017-04-07 22:54:46 +0800

    写好注册页的页面结构与样式

--- src/js/register.js
+++ src/js/register.js
@@ -5,12 +5,18 @@ new Vue({
     Password: "",
     Mobile: "",
     confirmPassword: "",
+    Email: "",
+    nickError: "",
+    pwdError: "",
+    confirmPwdError: "",
+    mobileError: "",
+    handlerError: "",
   },
   created() {
 
   },
   methods: {
-    save() {
+    register() {
       var data = {
         Nick: this.Nick,
         Password: this.Password,
@@ -31,6 +37,79 @@ new Vue({
           layer.msg(result.Message);
         }
       })
+    },
+    // 显示登录框
+    showLoginBox() {
+      this.layer = layer.open({
+        type: 1,
+        title: false,
+        area: "360px",
+        content: $("#loginbox"),
+        shadeClose: false,
+        closeBtn: 1,
+      });
+      // console.log(2333)
+    },
+    // 验证账号
+    checkAccount() {
+      if (this.Account === "" && this.Password !== "") {
+        this.errorInfor = "请输入账号";
+        this.isValid = false;
+      } else if (this.Account === "" && this.Password === "") {
+        this.errorInfor = "请输入账号和密码";
+        this.isValid = false;
+      } else {
+        this.errorInfor = "";
+      }
+    },
+    // 验证密码
+    checkPassword() {
+      if (this.Account !== "" && this.Password === "") {
+        this.errorInfor = "请输入密码";
+        this.isValid = false;
+      } else if (this.Account === "" && this.Password === "") {
+        this.errorInfor = "请输入账号和密码";
+        this.isValid = false;
+      } else {
+        this.errorInfor = "";
+      }
+    },
+    // 登录
+    login() {
+      var _this = this;
+      this.isValid = true;
+      this.checkAccount();
+      this.checkPassword();
+      if (this.isValid) {
+        var data = { Password: this.Password };
+        if (this.Account.length === 11) {
+          data.Nick = "";
+          data.Mobile = this.Account;
+        } else {
+          data.Nick = this.Account;
+          data.Mobile = "";
+        }
+        data = JSON.stringify(data);
+        fetch("/api/login", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            'Content-Type': "application/json"
+          },
+          body: data
+        }).then(res => res.json()).then(result => {
+          if (result.Code === 200) {
+            this.UsrName = result.Data;
+            localStorage.nick = this.UsrName;
+            layer.close(_this.layer)
+          } else {
+            this.errorInfor = result.Message;
+          }
+        }).catch(error => {
+          layer.msg("服务器错误，请稍后再试")
+          console.log(error)
+        })
+      }
     },
   }
 })
